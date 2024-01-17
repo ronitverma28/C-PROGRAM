7838a5380d134af1e4a83d711926e589df5cac9d
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-01-18 00:46:09 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main()
-{
+int main(){
     int arr[] = {10,5,6,2,1,15,8,9,7};
     int len = sizeof(arr) / sizeof(arr[0]);
-    int n = 5, i = 0;
+    int n, i = 0;
     bool search = false;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
-            if (arr[i] > arr[j])
-            {
+
+    printf("Enter the number to find in the array : ");
+    scanf("%d", &n);
+    for (int i = 0; i < len; i++){
+        for (int j = i + 1; j < len; j++){
+            if (arr[i] > arr[j]){
                 arr[i] += arr[j];
                 arr[j] = arr[i] - arr[j];
                 arr[i] -= arr[j];
             }
         }
     }
-    while (search = true)
-    {
-        if (arr[i] == n)
-        {
-            printf("%d is present at %d", n, i);
+    while (i<len){
+        if (arr[i] == n){
             search = true;
+            break;
         }
         i++;
     }
+    (search)?printf("%d is present at %d", n, i):printf("%d is not present in the array", n);
+
     return 0;
 }
