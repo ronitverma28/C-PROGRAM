d6bb027b01113979255bad2c78ef0348589115c4
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-01-20 00:37:54 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-int main(){
-    int arr[] = {10,9,5,1,2,3,6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    for(int i = 0;i<n;i++){
-        for(int k = i;k>0;k--){
-            if(arr[k] < arr[k-1]){
-                int temp = arr[k];
-                arr[k] = arr[k-1];
-                arr[k-1] = temp;
-            }
+#include <stdbool.h>
+int main() {
+    int arr[] = {10, 11, 16, 17, 2, 7, 1, 8, 5, 15, 12, 18, 9, 14, 13, 6, 4, 20, 19, 3};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int i,j, key;
+    
+    for(i = 0;i<len;i++){
+        key = arr[i];
+        j = i-1 ;
+        while(j>=0 && key < arr[j]){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = key;
     }
 
-    printf("Sorted array is\n");
-    for(int i = 0;i<n;i++){
+    printf("Sorted array : \n");
+    for(i = 0;i<len;i++){
         printf("%d ", arr[i]);
     }
+ 
     return 0;
 }
