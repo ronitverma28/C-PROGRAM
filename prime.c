7fc7df3c2355a69112fe2655d0358f4930888dc4
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-01-20 15:33:39 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int i, c = 0;
+    int n = printf("enter any number: ");
+    scanf("%d", &n);
+    for(i = 1;i<=n;i++){
+        if(n % i == 0){
+            c++;
+        }
+    }
+    (c==2)?printf("prime") : printf("not prime");
+
+    return 0;
+}
