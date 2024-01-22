288ed8c340e5b7e9a32312d9721972d33e955cd7
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-01-22 06:43:50 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdbool.h>
 int main() {
-    int arr[] = {10, 11, 16, 17, 2, 7, 1, 8, 5, 15, 12, 18, 9, 14, 13, 6, 4, 20, 19, 3};
+    int arr[] = {10, 11, 16, 17, 2, 7, 1, 8, 5, 19, 3};
     int len = sizeof(arr) / sizeof(arr[0]);
     int i,j, key;
-    
     for(i = 0;i<len;i++){
         key = arr[i];
         j = i-1 ;
@@ -19,6 +17,4 @@ int main() {
     for(i = 0;i<len;i++){
         printf("%d ", arr[i]);
     }
- 
-    return 0;
 }
