d6a7e0f9c910521c7d644b3a87572eb26bfcda27
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-06-24 01:06:38 -0700

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -37,3 +37,53 @@ int main() {
 
     return 0;
 }
+
+
+// method 2
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int binarySearch(int arr[], int length, int data)
+{
+    int left = 0, right = length - 1;
+    while (left <= right)
+    {
+        int mid = (left + right)/2;
+        if (data == arr[mid])
+        {
+            return mid;
+        }
+        else if (data < arr[mid])
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    // Write C code here
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    
+    int user_input;
+    printf("Enter Input: ");
+    scanf("%d", &user_input);
+    
+    int r = binarySearch(arr, length, user_input);
+    if (r == -1)
+    {
+        printf("not found");
+    }
+    else
+    {
+        printf("found: %d", r);
+    }
+
+    return 0;
+}
