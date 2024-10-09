19937a9367d3be1aca38b4b9c466394205a08645
RONIT KUMAR VERMA <131281559+ronitverma28@users.noreply.github.com>
2024-10-09 12:51:03 +0530

    Create python3.py

---
+++ python3.py
@@ -0,0 +1,56 @@
+sprite = Sprite('Tobi')
+# a = int(input("Enter the number: "))
+# b = int(input("Enter the number: "))
+
+# print(a+b)
+# print(a-b)
+# print(int(a/b))
+# print(a*b)
+# print(a%b)
+
+str = {
+  'Ronit': 95,
+  "Utkarsh": 120,
+  "Priyanshu": 86,
+  "Sourav": 108
+}
+
+myDict = {
+  'name': "Ronit",
+  'age': 19,
+  'course': "B-Tech",
+  'branch': "CSE",
+  'roll': 2302160100095,
+  'subjects': ("COA", "DSTL", "DS", "TC", "COE", "MATHS")
+}
+# print(myDict['subjects'][0])
+
+a = {1,2,3,4,5}
+b = {}
+
+
+while(input() != "y"):
+  b.add(int(input()))
+print(b)
+# if b in a:
+#   print("Yes")
+# else:
+#   print("No")
+
+# if evenOdd.lower() == "even":
+#   a = 2
+# else:
+#   a = 1
+
+# evenOdd = input("Enter the type: ")
+# n = int(input("Enter the n number: "))
+# a = 2 if evenOdd.lower() == "even" else 1
+# d = 2
+
+
+# sum = (n/2) * (2*a + (n-1)*d)
+# print(sum)
+
+
+
+
